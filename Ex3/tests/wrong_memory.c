3dfd4e37491599a48a0022057335265d26a66d3a
Lymperis Lymperidis <156132484+Lymperidis@users.noreply.github.com>
2024-03-09 14:36:53 +0200

    Excercise 3 System Call Implementation

---
+++ Ex3/tests/wrong_memory.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "d_params.h"
+
+    #define set_task_params(a,b) syscall(342, a, b)
+    #define get_task_params(a) syscall(343, a)
+        
+        
+        int main(void){
+        
+		struct d_params *params = (struct d_params *)malloc(1);
+		char group_name = 'Z';
+		int member_id = 25;
+		printf("Setting the params of struct_task : group_name=%c, member_id=%d\n",group_name,member_id);
+		set_task_params(group_name , member_id);
+		get_task_params(params);
+
+		                                
+		                                
+	return 0;
+}
+		                                                 
+		                                                 
